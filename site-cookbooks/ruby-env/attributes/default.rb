1cb3a0231ffc5a7790443fa63f4fe813d0cca79a
RyujiKawazoe <kawazoe@val.co.jp>
2015-02-24 13:04:11 +0900

    add rbenv-gem-rehash

--- site-cookbooks/ruby-env/attributes/default.rb
+++ site-cookbooks/ruby-env/attributes/default.rb
@@ -5,3 +5,6 @@ default['ruby-env']['rbenv_url'] = "https://github.com/sstephenson/rbenv"
 default['ruby-env']['ruby-build_url'] = "https://github.com/sstephenson/ruby-build"
 default['ruby-env']['rbenv']['branch'] = "master"
 default['ruby-env']['ruby-build']['branch'] = "master"
+
+#rbenv-gem-rehash
+default['ruby-env']['rbenv-gem-rehash_url'] = "https://github.com/sstephenson/rbenv-gem-rehash.git"
--- site-cookbooks/ruby-env/recipes/default.rb
+++ site-cookbooks/ruby-env/recipes/default.rb
@@ -45,6 +45,13 @@ git "/home/#{node['ruby-env']['user']}/.rbenv/plugins/ruby-build" do
   group node["ruby-env"]["group"]
 end
 
+git "/home/#{node['ruby-env']['user']}/.rbenv/plugins/rbenv-gem-rehash" do
+  repository node['ruby-env']['rbenv-gem-rehash_url']
+  action :sync
+  user node["ruby-env"]["user"]
+  group node["ruby-env"]["group"]
+end
+
 execute "rbenv install #{node['ruby-env']['version']}" do
   command "/home/#{node['ruby-env']['user']}/.rbenv/bin/rbenv install #{node['ruby-env']['version']}"
   user node['ruby-env']['user']
